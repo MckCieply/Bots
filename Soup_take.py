635e0bff6064f00b9e0c2faddeed0090fbf7ad72
Alex <mwppl123@gmail.com>
2022-07-09 18:57:26 +0200

    Working scrapper, only to insert into dsc bot

--- Soup_take.py
+++ Soup_take.py
@@ -4,24 +4,12 @@ import requests
 url = "https://www.formula1.com/en/results.html/2022/drivers.html" 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
-table = doc.find("table")
-td = table.find_all("td")
-driver = table.find_all("span")
-counter = 0
-for i in range (147):
-    if td[i].string is not None:
-        print(td[i].string, end = " ")
-        counter +=1
-        if counter == 3:
-            print("\n") 
-            counter = 0
-for i in range(63):
-    if driver[i].string is not None:
-        print(driver[i].string, end = " ")
-        counter += 1
-        if counter == 3:
-            print("\n") 
-            counter = 0
-# tabela = doc.find("table")
-# print(tabela)
-    
+print()
+for tr in doc.find_all('tr')[1:]:
+    first_name = tr.find('span', class_="hide-for-tablet").string
+    last_name = tr.find('span', class_ = "hide-for-mobile").string
+    car = tr.find('a', class_ = "grey semi-bold uppercase ArchiveLink").string
+    points = tr.find('td', class_ = "dark bold").string
+    print(f"""Driver: {first_name} {last_name}
+    Car: {car}
+    POINTS: {points}""")
